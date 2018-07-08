a27fcd1f7fd65aecf5df2daf2a37178a71fe172c
Jakub S <34782021+jakuski@users.noreply.github.com>
2018-07-08 19:31:01 +0100

    pretty much it I think

--- DoNotTrack/DoNotTrack.js
+++ DoNotTrack/DoNotTrack.js
@@ -7,14 +7,17 @@ module.exports = new Plugin({
 	description: "A port of the <a href=\"https://github.com/rauenzi/BetterDiscordAddons/blob/master/Plugins/DoNotTrack\" target=\"_blank\">Do Not Track</a> BD plugin by <a href=\"https://github.com/rauenzi/\" target=\"_blank\">Zerebros#7790</a>.",
 	author: "jakuski",
 	"color": "#d15353",
+	preload: false,
 	load: function () {
 		monkeyPatch( findModule("AnalyticEventConfigs"), "AnalyticEventConfigs", () => {});
 		monkeyPatch( findModule("consoleWarning"), "consoleWarning", () => {});
 		monkeyPatch( findModule("wrapMethod"), "wrapMethod", () => {});
+		monkeyPatch( findModule("_wrappedBuiltIns"), "_wrappedBuiltIns", () => {});
 	},
 	unload: function () {
 		findModule("AnalyticEventConfigs").AnalyticEventConfigs.unpatch();
 		findModule("consoleWarning").consoleWarning.unpatch();
 		findModule("wrapMethod").wrapMethod.unpatch();
+		findModule("_wrappedBuiltIns")._wrappedBuiltIns.unpatch();
 	}
 });
